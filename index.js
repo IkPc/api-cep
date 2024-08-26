678bf0a681a7b993bdddbb63a3c27601f110ad12
Pabllo Cristian <pabllo.dev@gmail.com>
2024-08-26 16:03:58 -0300

    fix api

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
-const form = document.forms['address'];
-const cepField = form['cep'];
-const rua = form['street'];
-const bairro = form['neighborhood'];
-const estado = form['state'];
-const cidade = form['city'];
-  
-cepField.addEventListener('blur', () => {
-    let cep = cepField.value
-  
-    if (/\d{5}-?\d{3}/.test(cep)) {
-      loadCepInfo(cep)
-    } else {
-      showCepError()
-    }
-})
+const cepField = document.getElementById('cep');
+const rua = document.getElementById('street');
+const numero = document.getElementById('number');
+const bairro = document.getElementById('neighborhood');
+const cidade = document.getElementById('city');
+const estado = document.getElementById('state');
 
-function getCep() {
-    let cep = document.querySelector('#cep').value
-    let url = `https://viacep.com.br/ws/${cep}/json/`;
-    let data = fetch(url)
-        .then(res => res.json())
-        /*.then(cepInfo => {
-            
-        })*/
-        .catch(error => {
-            showCepError()
-            return {erro: true}
-        })
-        if(data.erro) {
-            cleanAddressFields()
-        } else {
-            rua.value = data.logradouro
-            bairro.value = data.bairro
-            cidade.value = data.localidade
-            estado.value = data.uf
+cepField.addEventListener('change', () => {
+  let cepValue = cepField.value;
+  console.log('CEP Value:', cepValue);
 
-            numberField.focus()
-            cleanCepError()
-        }
+  if (/\d{5}-?\d{3}/.test(cepValue)) {
+    getCep(cepValue);
+  } else {
+    alert("CEP inválido!");
+  }
+});
+
+function getCep(cep) {
+  console.log('Fetching data for CEP:', cep); 
+  let url = `https://viacep.com.br/ws/${cep}/json/`;
+  fetch(url)
+    .then(res => res.json())
+    .then(cepInfo => {
+      console.log('CEP Info:', cepInfo);
+      if (cepInfo.erro) {
+        alert("Erro ao buscar o CEP.");
+      } else {
+        rua.value = cepInfo.logradouro;
+        bairro.value = cepInfo.bairro;
+        cidade.value = cepInfo.localidade;
+        estado.value = cepInfo.uf;
+        numero.focus();
+      }
+    })
+    .catch(error => {
+      alert("Erro ao buscar o CEP.");
+    });
 }
